72d9b0c731b3a770741ef9815cf6dd07aa6eb785
wanna <sangsangcat@gmail.com>
2026-04-28 17:08:44 +0900

    feat: 뉴스 아이템 클릭 시 외부 브라우저로 기사 링크 열기 기능 구현
    - url_launcher 패키지 추가 (pubspec.yaml)
    - NewsItemWidget: ListTile 클릭 시 외부 브라우저 호출 로직(launchUrl) 추가
    - Android: Android 11+ 대응을 위한 queries 설정 추가 (AndroidManifest.xml)
    - iOS: http/https 스킴 허용 설정 추가 (Info.plist)

--- lib/screens/detail/news_item_widget.dart
+++ lib/screens/detail/news_item_widget.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../models/news_article.dart';
 
 class NewsItemWidget extends StatelessWidget {
-  NewsArticle article;
+  final NewsArticle article;
 
   NewsItemWidget(this.article);
+
+  Future<void> _launchUrl() async {
+    final Uri url = Uri.parse(article.url);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } else {
+      debugPrint('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,9 +55,7 @@ class NewsItemWidget extends StatelessWidget {
           ),
         ],
       ),
-      onTap: (){
-
-      },
+      onTap: _launchUrl,
     );
   }
 }
